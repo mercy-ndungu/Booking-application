9ed20d10472ee1e201ef353a09e787ad3c284f67
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-11-05 22:53:15 +0300

    first commit

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE  DATABASE bookingapp;
+CREATE TABLE bookingapp{
+    booking_id SERIAL PRIMARY KEY,
+    
+
+}
---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+// import cors from 'cors';
+const app = express();
+// const cors = require('cors');
+
+app.listen(8000, () => {
+    console.log("Server listening on port");
+})

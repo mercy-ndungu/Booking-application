cae068f84be94fd63d2946ed89185084a60101c6
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-11-14 00:01:09 +0300

    Implemented user authentication

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,7 @@
-CREATE  DATABASE bookingapp;
-CREATE TABLE bookingapp{
-    booking_id SERIAL PRIMARY KEY,
+-- CREATE  DATABASE bookingapp;
+-- CREATE TABLE bookingapp{
+--     booking_id SERIAL PRIMARY KEY,
     
 
-}
+-- }
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
